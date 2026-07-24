bd2bdbcd0a7f43af6079b69f3d792ff1ba214e5b
ANIakovlev <ANIakovlev@synergy.ru>
2026-07-24 21:16:33 +0300

    feat: добавить скролл наверх при смене роута

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -3,7 +3,7 @@
 		<Header />
 		<div class="flex grow min-h-0">
 			<Menu class="hidden lg:flex" preset="desk" />
-			<main class="flex flex-col scroll-block grow text-white p-16 overflow-auto">
+			<main ref="content" class="flex flex-col scroll-block grow text-white p-16 overflow-auto">
 				<NuxtPage />
 			</main>
 		</div>
@@ -12,4 +12,13 @@
 <script setup lang="ts">
 import Header from "@/components/molecules/layout/Header.vue";
 import Menu from "@/components/molecules/layout/Menu.vue";
+
+const router = useRouter();
+
+const content = useTemplateRef("content");
+
+router.afterEach(async () => {
+	await nextTick();
+	content.value?.scrollTo({ behavior: "smooth", top: 0 });
+});
 </script>
