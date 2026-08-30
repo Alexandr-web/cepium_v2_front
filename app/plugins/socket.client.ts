079536d99c3182d89e8319ceef90833b5cf657d4
ANIakovlev <ANIakovlev@synergy.ru>
2026-08-30 11:57:55 +0300

    fix: поправить обработку ошибок с сокета

--- app/plugins/socket.client.ts
+++ app/plugins/socket.client.ts
@@ -80,7 +80,7 @@ export default defineNuxtPlugin(() => {
 
 		// обработка ошибок
 		socket.on("accountInfoError", (data) => {
-			const message = parseExchangeErrorMessage(data, exchangeStore.activeExchange ?? "");
+			const message = parseExchangeErrorMessage(data.message, exchangeStore.activeExchange ?? "");
 			
 			if (message) {
 				connectionStore.errorMessage = message;
