61f669e8e044955e3d3e7e0d1fa12a2b4d365fa8
ANIakovlev <ANIakovlev@synergy.ru>
2026-09-10 14:10:25 +0300

    feat: обновить compatibilityDate

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-	compatibilityDate: "2025-07-15",
+	compatibilityDate: "2026-09-10",
 	app: {
 		head: {
 			title: "Cepium",
